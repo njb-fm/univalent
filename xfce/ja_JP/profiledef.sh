234a87f80dfcc419f6504c4ed518b18dc21bff25
Jin Asanami <101626649+Jin-Asanami@users.noreply.github.com>
2022-06-23 20:19:29 +0900

    Update profiledef.sh

--- xfce/ja_JP/profiledef.sh
+++ xfce/ja_JP/profiledef.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-os_name="Univalent GNU/Linux"
 iso_name="uvgl-xfce-ja"
 iso_label="UNIVALENT_XFCE_JA_$(date +%Y%m%d)_ALPHA"
 iso_publisher="RadioNewJapan"
