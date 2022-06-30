8627db5ba77d89625675fa9bcda9ee78ffe76d17
Jin Asanami <101626649+Jin-Asanami@users.noreply.github.com>
2022-06-30 23:17:28 +0900

    Migrated LXQt edition to gamma version

--- lxqt/en_US/profiledef.sh
+++ lxqt/en_US/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uvgl-lxqt-en"
-iso_label="UNIVALENT_LXQT_EN_$(date +%Y%m%d)_BETA"
+iso_label="UNIVALENT_LXQT_EN_$(date +%Y%m%d)_GAMMA"
 iso_publisher="RadioNewJapan"
 iso_application="UVGL Install Drive"
-iso_version="$(date +%Y.%m.%d).b"
+iso_version="$(date +%Y.%m.%d).g"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
