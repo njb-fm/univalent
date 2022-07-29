c2e6128b3966f4fa4cfa64eceab5693db8e1cd02
Jin-Asanami <is01.njb@gmail.com>
2022-07-29 17:32:02 +0900

    change xfce from beta to gamma

--- xfce/ja_JP/profiledef.sh
+++ xfce/ja_JP/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uvgl-xfce-ja"
-iso_label="UNIVALENT_XFCE_JA_$(date +%y%m%d)_BETA2"
+iso_label="UNIVALENT_XFCE_JA_$(date +%y%m%d)_GAMMA1"
 iso_publisher="RadioNewJapan"
 iso_application="UVGL Install Drive"
-iso_version="$(date +%y.%m.%d).b2"
+iso_version="$(date +%y.%m.%d).g1"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
