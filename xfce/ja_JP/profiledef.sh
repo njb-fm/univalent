3961ee3bdfc73e82f3f9ba96c73a0e633727238f
Jin-Asanami <is01.njb@gmail.com>
2022-08-09 22:34:45 +0900

    (xfce) fix shortcut keys

--- xfce/ja_JP/profiledef.sh
+++ xfce/ja_JP/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uvgl-xfce-ja"
-iso_label="UVGL_XFCE_JA_$(date +%y%m%d)_GAMMA1"
+iso_label="UVGL_XFCE_JA_$(date +%y%m%d)_GAMMA2"
 iso_publisher="RadioNewJapan"
 iso_application="UVGL Install Drive"
-iso_version="$(date +%y.%m.%d).g1"
+iso_version="$(date +%y.%m.%d).g2"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
