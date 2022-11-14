21ac44b91878fcaaf96ed71dd8c621aaf5e7cf42
Jin-Asanami <is01.njb@gmail.com>
2022-11-14 19:44:28 +0900

    (all) add gedit instead of leafpad

--- plasma/ja_JP/profiledef.sh
+++ plasma/ja_JP/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uvgl-plasma-ja"
-iso_label="UVGL_PLASMA_JA_$(date +%y.%m).1"
+iso_label="UVGL_PLASMA_JA_$(date +%y.%m)"
 iso_publisher="RadioNewJapan"
 iso_application="UVGL Installation Drive"
-iso_version="$(date +%y.%m).1"
+iso_version="$(date +%y.%m)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
