be8affcd0adf1547898f74d60e639eccab133268
Jin-Asanami <is01.njb@gmail.com>
2022-11-26 13:47:27 +0900

    (g) release alpha-3

--- gnome/en_US/profiledef.sh
+++ gnome/en_US/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uvgl-gnome-en"
-iso_label="UVGL_GNOME_EN_$(date +%y%m%d)_ALPHA2"
+iso_label="UVGL_GNOME_EN_$(date +%y%m%d)_ALPHA3"
 iso_publisher="RadioNewJapan"
 iso_application="UVGL Installation Drive"
-iso_version="$(date +%y.%m.%d).a2"
+iso_version="$(date +%y.%m.%d).a3"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
