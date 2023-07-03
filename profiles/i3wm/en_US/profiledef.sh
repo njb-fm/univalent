06b9e1f3b0b92bbc481ef310a252bbde3dd58aa1
jin-asanami <is01.njb@gmail.com>
2023-07-03 21:18:33 +0900

    release 23.07 (i3wm only)

--- profiles/i3wm/en_US/profiledef.sh
+++ profiles/i3wm/en_US/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="uni3valent-en"
-iso_label="UVE_2306I"
+iso_label="UVE_2307I"
 iso_publisher="RadioNewJapan"
 iso_application="UVE Installaton Drive"
-iso_version="23.06.1"
+iso_version="23.07"
 install_dir="univa"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
